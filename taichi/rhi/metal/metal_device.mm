b4b8df5ad176444623f0200f30268f6f6f70b01f
Hugh Perkins <hughperkins@gmail.com>
2025-04-26 12:57:01 -0400

    modifications for mac m4 seqouia

--- taichi/rhi/metal/metal_device.mm
+++ taichi/rhi/metal/metal_device.mm
@@ -1,8 +1,21 @@
-#include "taichi/rhi/metal/metal_device.h"
+#include <math.h>
+
+extern "C" {
+void __sincospif(float x, float *sinp, float *cosp) {
+  *sinp = sinf(M_PI * x);
+  *cosp = cosf(M_PI * x);
+}
+void __sincospi(double x, double *sinp, double *cosp) {
+  *sinp = sin(M_PI * x);
+  *cosp = cos(M_PI * x);
+}
+}
+
 #include "spirv_msl.hpp"
 #include "taichi/rhi/device.h"
 #include "taichi/rhi/device_capability.h"
 #include "taichi/rhi/impl_support.h"
+#include "taichi/rhi/metal/metal_device.h"
 
 namespace taichi::lang {
 namespace metal {
