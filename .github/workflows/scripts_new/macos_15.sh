282f493dc9434505a069cf8652bf3584165655f5
Hugh Perkins <hughperkins@gmail.com>
2025-05-20 20:01:52 -0400

    update scripts

--- .github/workflows/scripts_new/macos_15.sh
+++ .github/workflows/scripts_new/macos_15.sh
@@ -3,3 +3,14 @@
 set -ex
 
 echo hello from macos_15.sh
+pwd
+sw_vers
+clang++ --version
+uname -a
+clang --version
+# free -m
+ls -la
+python -V
+brew install molten-vk
+brew install llvm@15
+./build.py --shell
