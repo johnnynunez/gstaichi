0db90f985f1b6ffcef985fc805be6e06acf7ca6e
Hugh Perkins <hughperkins@gmail.com>
2025-05-20 20:38:18 -0400

    reenable mac build

--- .github/workflows/scripts_new/macos_15.sh
+++ .github/workflows/scripts_new/macos_15.sh
@@ -4,8 +4,9 @@ set -ex
 
 echo hello from macos_15.sh
 pwd
+git submodule 
+git submodule update --init --recursive
 brew list
-# brew uninstall python@3.13
 brew install python@3.10
 brew link --force python@3.10
 which python
@@ -14,18 +15,14 @@ sudo rm /opt/homebrew/bin/python3
 sudo rm /Library/Frameworks/Python.framework/Versions/Current/bin/python
 sudo ln -s /opt/homebrew/bin/python3.10 /opt/homebrew/bin/python3
 sudo ln -s /opt/homebrew/bin/python3.10 /Library/Frameworks/Python.framework/Versions/Current/bin/python
-# export PATH="/opt/homebrew/:$PATH"
-# export PATH="/opt/homebrew/opt/python@3.10/bin:$PATH"
-# brew unlink --force python@3.13
 python --version
 sw_vers
 clang++ --version
 uname -a
 clang --version
-# free -m
 ls -la
 python -V
-# brew install molten-vk
-# brew install llvm@15
-# ./build.py --shell
-# python setup.py develop
+brew install molten-vk
+brew install llvm@15
+./build.py --shell
+python setup.py develop
