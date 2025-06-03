c98ef0b104abaaf9db1dcc1f20e31b35d24f9de0
Hugh Perkins <hughperkins@gmail.com>
2025-06-02 20:03:32 -0700

    install prerequiestis for build

--- .github/workflows/scripts_new/clang_tidy.sh
+++ .github/workflows/scripts_new/clang_tidy.sh
@@ -10,6 +10,34 @@
 sudo apt-get update
 sudo apt-get install -y clang-tidy-18
 git submodule update --init --recursive
+
+sudo apt install -y \
+    freeglut3-dev \
+    libglfw3-dev \
+    libglm-dev \
+    libglu1-mesa-dev \
+    libwayland-dev \
+    libx11-xcb-dev \
+    libxcb-dri3-dev \
+    libxcb-ewmh-dev \
+    libxcb-keysyms1-dev \
+    libxcb-randr0-dev \
+    libxcursor-dev \
+    libxi-dev \
+    libxinerama-dev \
+    libxrandr-dev \
+    pybind11-dev \
+    libc++-15-dev \
+    libc++abi-15-dev \
+    clang-15 \
+    libclang-common-15-dev \
+    libclang-cpp15 \
+    libclang1-15 \
+    cmake \
+    ninja-build \
+    python3-dev \
+    python3-pip
+
 pip install scikit-build
 export TAICHI_CMAKE_ARGS="-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 ./build.py wheel
