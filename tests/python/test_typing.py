041d4a93e6e4beaac7368d002278dfc02d2504b3
Hugh Perkins <hughperkins@gmail.com>
2025-06-10 15:59:45 -0700

    [Type] Allow none return typing, for kernels and funcs (#18)
    * allow `-> None` for kernels
    
    * add unit test

---
+++ tests/python/test_typing.py
@@ -0,0 +1,28 @@
+import test_utils
+import taichi as ti
+
+
+@test_utils.test()
+def test_typing_kernel_return_none():
+    x = ti.field(ti.i32, shape=())
+
+    @ti.kernel
+    def some_kernel() -> None:
+        x[None] += 1
+
+    some_kernel()
+
+
+@test_utils.test()
+def test_typing_func_return_none():
+    x = ti.field(ti.i32, shape=())
+
+    @ti.func
+    def some_func() -> None:
+        x[None] += 1
+
+    @ti.kernel
+    def some_kernel() -> None:
+        some_func()
+
+    some_kernel()
