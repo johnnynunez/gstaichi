a5dff663757f717e19ad696b9d65f4018cd1b1b1
Hugh Perkins <hughperkins@gmail.com>
2025-07-02 09:01:22 -0700

    [Build] Add pyi stubs to wheel build (#49)
    * manylinux wheel
    
    * .yml
    
    * needs
    
    * make w safe
    
    * precommit
    
    * more exploration
    
    * try different wheel path
    
    * save stuff I"m trying
    
    * setting python version
    
    * udpate python path
    
    * fix path
    
    * maybe fix python path
    
    * run repair
    
    * remove -v
    
    * remove yum update
    
    * push to s3
    
    * find both clang and clang++
    
    * download clang; skip compiler checks
    
    * install wget
    
    * -q
    
    * make apple defintiosn conditional on apple
    
    * always set the -Wno flag
    
    * link libstdc++.so, so can be found easily inchecks
    
    * install static libstdc++ lib
    
    * remove android thing, since probalby old, and eigen wont compile anyway without it
    
    * remove dead path line
    
    * -y
    
    * /wheelhouse
    
    * lst dist/*.whl
    
    * hunt fo the wheel
    
    * upload from wheelhouse
    
    * try not using special llvm
    
    * fix sync command
    
    * addd stdc++fs
    
    * remove dead code
    
    * remove more dead code
    
    * remove some scaffolding
    
    * run py 3.10 -> 3.13
    
    * remove some more spam
    
    * fail-fast: false
    
    * add setuptools and cffi to install requires
    
    * add seutptools and cffi to pyproject.toml
    
    * remove wget
    
    * add pyi to wheel
    
    * add test of typing
    
    * pre-commit
    
    * ruff
    
    * enable vulkan
    
    * -v -r 3
    
    * Revert "enable vulkan"
    
    This reverts commit 374a6582ac2637a16c0d6c493d2822841469b437.
    
    * add __init__.py so can continue to build with develop
    
    * fix tset_pyi_stubs to use pyright intead of mypy
    
    * address review comments

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ from distutils.dir_util import remove_tree
 from setuptools import find_packages
 from skbuild import setup
 from skbuild.command.egg_info import egg_info
+from wheel.bdist_wheel import bdist_wheel
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -107,6 +108,30 @@ class Clean(clean):
                 print(f"removing generated file {f}")
                 if not self.dry_run:
                     os.remove(f)
+
+
+class BDistWheelWithStubs(bdist_wheel):
+    def run(self):
+        build_lib = self.get_finalized_command("build_py").build_lib
+        taichi_path = os.path.join(os.path.dirname(os.path.dirname(build_lib)), "cmake-install/python")
+        env = os.environ.copy()
+        env["PYTHONPATH"] = taichi_path + os.pathsep + env.get("PYTHONPATH", "")
+
+        # command that works:
+        # PYTHONPATH=_skbuild/linux-x86_64-3.10/cmake-install/python pybind11-stubgen \
+        #     taichi._lib.core.taichi_python --ignore-all-errors
+        cmd_line = ["pybind11-stubgen", "taichi._lib.core.taichi_python", "--ignore-all-errors"]
+        print(" ".join(cmd_line))
+        subprocess.check_call(cmd_line, env=env)
+        stub_filepath = "stubs/taichi/_lib/core/taichi_python.pyi"
+        target_filepath = os.path.join(build_lib, "taichi/_lib/core/taichi_python.pyi")
+        py_typed_dst = os.path.join(build_lib, "taichi/_lib/core/py.typed")
+        os.makedirs(os.path.dirname(target_filepath), exist_ok=True)
+        print("copying ", stub_filepath, "to", target_filepath)
+        shutil.copy(stub_filepath, target_filepath)
+        with open(py_typed_dst, "w"):
+            pass  # creates an empty file
+        super().run()
 
 
 def get_cmake_args():
@@ -234,6 +259,9 @@ setup(
     data_files=[
         (os.path.join("_lib", "runtime"), data_files),
     ],
+    package_data={
+        "taichi._lib.core": ["taichi_python.pyi", "py.typed"],
+    },
     keywords=["graphics", "simulation"],
     license="Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)",
     include_package_data=True,
@@ -245,7 +273,11 @@ setup(
     classifiers=classifiers,
     cmake_args=get_cmake_args(),
     cmake_process_manifest_hook=cmake_install_manifest_filter,
-    cmdclass={"egg_info": EggInfo, "clean": Clean},
+    cmdclass={
+        "egg_info": EggInfo,
+        "clean": Clean,
+        "bdist_wheel": BDistWheelWithStubs,
+    },
     has_ext_modules=lambda: True,
 )
 
