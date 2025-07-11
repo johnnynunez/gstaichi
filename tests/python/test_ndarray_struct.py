1b7f6a57b80c2429a434baac6bf906cbb45a530e
Hugh Perkins <hughperkins@gmail.com>
2025-07-10 19:34:29 -0700

    add unit test with fields in structs

--- tests/python/test_ndarray_struct.py
+++ tests/python/test_ndarray_struct.py
@@ -123,6 +123,123 @@ def test_ndarray_struct_l4():
 
 
 @test_utils.test()
+def test_ndarray_struct_field_l4():
+    gc.collect()
+    gc.collect()
+    a = ti.field(ti.i32, shape=(55,))
+    b = ti.field(ti.i32, shape=(57,))
+    c = ti.field(ti.i32, shape=(211,))
+    d = ti.field(ti.i32, shape=(223,))
+    e = ti.field(ti.i32, shape=(227,))
+
+    @dataclass
+    class MyStruct:
+        a: ti.template()
+        b: ti.template()
+        c: ti.template()
+
+    @ti.func
+    def s3(z3: ti.template(), my_struct3: MyStruct, bar3: ti.template()) -> None:
+        # stores
+        z3[25] += 90
+        my_struct3.a[47] += 42
+        my_struct3.b[49] += 43
+        my_struct3.c[43] += 44
+        bar3[113] += 125
+
+        # loads
+        bar3[16] = z3[1]
+        my_struct3.a[17] = z3[1]
+        my_struct3.b[18] = my_struct3.a[3]
+        my_struct3.c[19] = my_struct3.b[18]
+        z3[20] = my_struct3.c[5]
+
+    @ti.func
+    def s2(z3: ti.template(), my_struct3: MyStruct, bar3: ti.template()) -> None:
+        # stores
+        z3[24] += 89
+        my_struct3.a[46] += 32
+        my_struct3.b[48] += 33
+        my_struct3.c[42] += 34
+        bar3[112] += 125
+        s3(z3, my_struct3, bar3)
+
+    @ti.func
+    def s1(z2: ti.template(), my_struct2: MyStruct, bar2: ti.template()) -> None:
+        # stores
+        z2[22] += 88
+        my_struct2.a[45] += 22
+        my_struct2.b[47] += 23
+        my_struct2.c[41] += 24
+        bar2[111] += 123
+        s2(z2, my_struct2, bar2)
+
+    @ti.kernel
+    def k1(z: ti.template(), my_struct: MyStruct, bar: ti.template()) -> None:
+        # stores
+        z[33] += 2
+        my_struct.a[35] += 3
+        my_struct.b[37] += 5
+        my_struct.c[51] += 17
+        bar[222] = 41
+
+        # loads
+        bar[6] = z[1]
+        my_struct.a[7] = z[1]
+        my_struct.b[8] = my_struct.a[3]
+        my_struct.c[9] = my_struct.b[8]
+        z[10] = my_struct.c[5]
+        s1(z, my_struct, bar)
+
+    d[1] = 11
+    a[3] = 12
+    b[2] = 13
+    c[5] = 14
+    e[4] = 15
+
+    my_struct = MyStruct(a=a, b=b, c=c)
+    k1(d, my_struct, e)
+    # store tests k1
+    assert d[33] == 2
+    assert a[35] == 3
+    assert b[37] == 5
+    assert c[51] == 17
+
+    # from load tests, k1
+    assert e[6] == 11
+    assert a[7] == 11
+    assert b[8] == 12
+    assert c[9] == 12
+    assert d[10] == 14
+
+    assert d[22] == 88
+    assert a[45] == 22
+    assert b[47] == 23
+    assert c[41] == 24
+    assert e[111] == 123
+
+    assert d[24] == 89
+    assert a[46] == 32
+    assert b[48] == 33
+    assert c[42] == 34
+    assert e[112] == 125
+
+    # s3 stores
+    assert d[25] == 90
+    assert a[47] == 42
+    assert b[49] == 43
+    assert c[43] == 44
+    assert e[113] == 125
+
+    # s3 loads
+    assert e[16] == 11
+    assert a[17] == 11
+    assert b[18] == 12
+    assert c[19] == 12
+    assert d[20] == 14
+
+
+@test_utils.test()
 def test_ndarray_struct_diverse_params():
     gc.collect()
     gc.collect()
