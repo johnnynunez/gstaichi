c08ba97fbca77e2835143c30147a92bdd21c4c46
Hugh Perkins <hughperkins@gmail.com>
2025-07-16 16:58:11 -0700

    precommit test_kernel_impl_dataclass

--- tests/python/taichi/lang/test_kernel_impl_dataclass.py
+++ tests/python/taichi/lang/test_kernel_impl_dataclass.py
@@ -2,34 +2,33 @@
 I want to migrate all tests to match the directory structure of the code under test,
 but let's just start with a few tests using this structure/architecture, as a prototype/PoC.
 """
-from tests import test_utils
-import ast
-import pytest
-from ast import (
-    Attribute,
-    Name,
-    Load
-)
-import taichi.lang._kernel_impl_dataclass as _kernel_impl_dataclass
 
+import ast
+from ast import Attribute, Load, Name
+
+import pytest
+
+import taichi.lang._kernel_impl_dataclass as _kernel_impl_dataclass
+from tests import test_utils
 
 __all__ = [
-    'Attribute',
-    'Name',
-    'Load',
+    "Attribute",
+    "Name",
+    "Load",
 ]
 
 
 @pytest.mark.parametrize(
-    "ast_in, struct_locals, expected_ast", [
+    "ast_in, struct_locals, expected_ast",
+    [
         (
             """
     Attribute(
         value=Name(id='my_struct_ab', ctx=Load()),
         attr='a',
         ctx=Load())""",
-        {"__ti_my_struct_ab__ti_a"},
-        "Name(id='__ti_my_struct_ab__ti_a', ctx=Load())"
+            {"__ti_my_struct_ab__ti_a"},
+            "Name(id='__ti_my_struct_ab__ti_a', ctx=Load())",
         ),
         (
             """
@@ -41,8 +40,8 @@ __all__ = [
         attr='d',
         ctx=Load())
 """,
-        {"__ti_my_struct_ab__ti_struct_cd__ti_d"},
-        "Name(id='__ti_my_struct_ab__ti_struct_cd__ti_d', ctx=Load())"
+            {"__ti_my_struct_ab__ti_struct_cd__ti_d"},
+            "Name(id='__ti_my_struct_ab__ti_struct_cd__ti_d', ctx=Load())",
         ),
         (
             """
@@ -56,10 +55,10 @@ __all__ = [
             ctx=Load()),
         attr='f',
         ctx=Load())""",
-        {"__ti_my_struct_ab__ti_struct_cd__ti_struct_ef__ti_f"},
-        "Name(id='__ti_my_struct_ab__ti_struct_cd__ti_struct_ef__ti_f', ctx=Load())"
+            {"__ti_my_struct_ab__ti_struct_cd__ti_struct_ef__ti_f"},
+            "Name(id='__ti_my_struct_ab__ti_struct_cd__ti_struct_ef__ti_f', ctx=Load())",
         ),
-    ]
+    ],
 )
 @test_utils.test()
 def test_unpack_ast_struct_expressions(ast_in: str, struct_locals: set[str], expected_ast: str) -> None:
