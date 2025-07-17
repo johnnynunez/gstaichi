dc4c8e376f152902afc409ec4d73c03d39d0c835
Hugh Perkins <hughperkins@gmail.com>
2025-07-17 15:34:41 -0700

    bilding test

---
+++ tests/python/taichi/lang/ast/test_function_def_transformer.py
@@ -0,0 +1,83 @@
+from typing import Any, cast
+import pytest
+import taichi as ti
+import ast
+import pytest
+from taichi.lang.ast.function_def_transformer import FunctionDefTransformer
+from tests import test_utils
+import dataclasses
+
+
+@dataclasses.dataclass
+class MyStructAB:
+    a: ti.types.NDArray[ti.i32, 1]
+    b: ti.types.NDArray[ti.i32, 1]
+
+
+@dataclasses.dataclass
+class MyStructCD:
+    c: ti.types.NDArray[ti.i32, 1]
+    d: ti.types.NDArray[ti.i32, 1]
+    my_struct_ab: MyStructAB
+
+
+@dataclasses.dataclass
+class MyStructEF:
+    e: ti.types.NDArray[ti.i32, 1]
+    f: ti.types.NDArray[ti.i32, 1]
+    my_struct_cd: MyStructCD
+
+
+class NDArrayBuilder:
+    def __init__(self, dtype: Any, shape: tuple[int, ...]) -> None:
+        self.dtype = dtype
+        self.shape = shape
+    
+    def build(self) -> ti.types.NDArray:
+        return ti.ndarray(self.dtype, self.shape)
+
+
+def build_struct(struct_type: Any) -> Any:
+    for field in dataclasses.fields(struct_type):
+    member_objects = {}
+    for field in dataclasses.fields(struct_type):
+        if field.type == ti.types.ndarray:
+            print("got ndarray")
+            ndarray_type = cast(ti.types.ndarray, field.type)
+            child = ti.ndarray()
+    # DataclassClass = dataclasses.make_dataclass(struct_name, declaration_type_by_name)
+    dataclass_object = struct_type(**arrays)
+
+
+@pytest.mark.parametrize(
+    "argument_name, argument_type, expected_variables", [
+        (
+            "my_struct_1",
+            MyStructAB,
+            # MyStructAB(e=ti.ndarray(ti.i32, (10,)), f=ti.ndarray(ti.i32, (20,))),
+            {
+                "__ti_my_struct_1__ti_a": ti.ndarray(ti.i32, (10,)),
+                "__ti_my_struct_1__ti_b": ti.ndarray(ti.i32, (20,)),
+            }
+        ),
+    ]
+)
+@test_utils.test()
+def test_process_func_arg(argument_name: str, argument_type: Any, data: Any, expected_variables: dict[str, Any]) -> None:
+    class MockContext:
+        def __init__(self) -> None:
+            self.variables: dict[str, Any] = {}
+
+        def create_variable(self, name: str, data: Any) -> None:
+            assert name not in self.variables
+            self.variables[name] = data
+    
+    ctx = MockContext()
+    FunctionDefTransformer._process_func_arg(
+        ctx,
+        argument_name,
+        argument_type,
+        data,
+    )
+    print("output variables", ctx.variables)
+    print("epected variables", expected_variables)
