72224fa1d6db2c7903d59ffbd100d99757523107
Hugh Perkins <hughperkins@gmail.com>
2025-07-16 18:32:11 -0700

    add new test (currently failing)

--- tests/python/taichi/lang/ast/test_call_transformer.py
+++ tests/python/taichi/lang/ast/test_call_transformer.py
@@ -12,6 +12,13 @@ class MyStructAB:
     b: ti.types.NDArray[ti.i32, 1]
 
 
+@dataclasses.dataclass
+class MyStructCD:
+    c: ti.types.NDArray[ti.i32, 1]
+    d: ti.types.NDArray[ti.i32, 1]
+    my_struct_ab: MyStructAB
+
+
 def dump_ast_list(nodes: list[ast.AST]) -> str:
     res_l = []
     for node in nodes:
@@ -22,13 +29,24 @@ def dump_ast_list(nodes: list[ast.AST]) -> str:
 @pytest.mark.parametrize(
     "args_in, expected_args_out", [
         (
-        [
-            ast.Name(id='my_struct_ab', ctx=ast.Load(), ptr=MyStructAB)
-        ],
-        [
-            ast.Name(id='__ti_my_struct_ab__ti_a', ctx=ast.Load()),
-            ast.Name(id='__ti_my_struct_ab__ti_b', ctx=ast.Load()),
-        ]
+            [
+                ast.Name(id='my_struct_ab', ctx=ast.Load(), ptr=MyStructAB)
+            ],
+            [
+                ast.Name(id='__ti_my_struct_ab__ti_a', ctx=ast.Load()),
+                ast.Name(id='__ti_my_struct_ab__ti_b', ctx=ast.Load()),
+            ]
+        ),
+        (
+            [
+                ast.Name(id='my_struct_cd', ctx=ast.Load(), ptr=MyStructCD)
+            ],
+            [
+                ast.Name(id='__ti_my_struct_cd__ti_c', ctx=ast.Load()),
+                ast.Name(id='__ti_my_struct_cd__ti_d', ctx=ast.Load()),
+                ast.Name(id='__ti_my_struct_cd__ti_my_struct_ab__ti_a', ctx=ast.Load()),
+                ast.Name(id='__ti_my_struct_cd__ti_my_struct_ab__ti_b', ctx=ast.Load()),
+            ]
         ),
     ]
 )
@@ -39,23 +57,8 @@ def test_expand_Call_dataclass_args(args_in: list[ast.AST], expected_args_out: l
         arg.endlineno = 2
         arg.col_offset = 1
         arg.end_col_offset = 2
-    # args_in_obj = ast.Call(
-    #     func=ast.Name(id='f2', ctx=ast.Load()),
-    #     args=[
-    #         ast.Name(id='my_struct_b', ctx=ast.Load(), ptr=123)
-    #     ],
-    #     keywords=[]
-    # )
-    print("args_in", dump_ast_list(args_in))
-    # print("args_in_obj", ast.dump(args_in, indent=2))
-    print("args_in_obj.args[0].ptr", args_in[0].ptr)
-    # args_in_obj = eval(args_in)
-    # expected_args_out_obj = eval(expected_args_out)
     args_out = CallTransformer._expand_Call_dataclass_args(args_in)
     print("args_in", dump_ast_list(args_in))
     print("args_out", dump_ast_list(args_out))
     print("expected_args_out", dump_ast_list(expected_args_out))
-    # print("args_in", ast.dump(args_in, indent=2))
-    # print("expected_args", ast.dump(expected_args_out, indent=2))
-    # print("args_out", ast.dump(args_out, indent=2))
     assert dump_ast_list(expected_args_out) == dump_ast_list(args_out)
