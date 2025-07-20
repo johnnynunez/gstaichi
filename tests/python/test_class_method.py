8de645a9be671be53844b61760214ea6713a8efb
YilingQiao <yilingq97@gmail.com>
2025-07-20 15:12:53 -0700

    add a test for calling class method

---
+++ tests/python/test_class_method.py
@@ -0,0 +1,44 @@
+import taichi as ti
+from tests import test_utils
+
+
+@test_utils.test()
+def test_class_method():
+    use_ndarray = False
+
+    if not use_ndarray:
+        V = ti.field
+        V_ANNOTATION = ti.template()
+    else:
+        V = ti.ndarray
+        V_ANNOTATION = ti.types.ndarray()
+
+    shape = (20,)
+
+    @ti.data_oriented
+    class InnerClass:
+        def __init__(self):
+            self.a = V(ti.i32, shape)
+            self.b = V(ti.i32, shape)
+
+    @ti.data_oriented
+    class MyClass:
+        def __init__(self):
+            self.inner = InnerClass()
+            self.c = V(ti.i32, shape)
+            self.d = V(ti.i32, shape)
+            self.e = V(ti.i32, shape)
+
+        @ti.func
+        def f1(self_unused, c: V_ANNOTATION, inner: ti.template()):
+            c[0] = 4
+
+        @ti.kernel
+        def test(self, c: V_ANNOTATION, inner: ti.template()):
+            self.f1(c, inner=inner)
+
+        def run(self):
+            self.test(self.c, self.inner)
+
+    obj = MyClass()
+    obj.run()
