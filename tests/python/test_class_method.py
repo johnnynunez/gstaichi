09a5fbc38e50aaca1b5ca0527c0846949d242c4d
YilingQiao <49262224+YilingQiao@users.noreply.github.com>
2025-07-25 02:39:42 -0400

    [Unittest] Add a test to cover calling class method (#96)
    * add a test for calling class method
    
    * use_ndarray parameterize; return type
    
    * adding local fixtures
    
    ---------
    
    Co-authored-by: Hugh Perkins <hughperkins@gmail.com>

---
+++ tests/python/test_class_method.py
@@ -0,0 +1,54 @@
+from typing import Any
+
+import pytest
+
+import taichi as ti
+from tests import test_utils
+
+
+@pytest.fixture
+def ti_type(use_ndarray: bool) -> Any:
+    if use_ndarray:
+        return ti.ndarray
+    return ti.field
+
+
+@pytest.fixture
+def ti_annotation(use_ndarray: bool) -> Any:
+    if use_ndarray:
+        return ti.types.ndarray()
+    return ti.template()
+
+
+@pytest.mark.parametrize("use_ndarray", [False, True])
+@test_utils.test()
+def test_class_method(ti_type: Any, ti_annotation: Any) -> None:
+    shape = (20,)
+
+    @ti.data_oriented
+    class InnerClass:
+        def __init__(self):
+            self.a = ti_type(ti.i32, shape)
+            self.b = ti_type(ti.i32, shape)
+
+    @ti.data_oriented
+    class MyClass:
+        def __init__(self):
+            self.inner = InnerClass()
+            self.c = ti_type(ti.i32, shape)
+            self.d = ti_type(ti.i32, shape)
+            self.e = ti_type(ti.i32, shape)
+
+        @ti.func
+        def f1(self_unused, c: ti_annotation, inner: ti.template()):
+            c[0] = 4
+
+        @ti.kernel
+        def test(self, c: ti_annotation, inner: ti.template()):
+            self.f1(c, inner=inner)
+
+        def run(self):
+            self.test(self.c, self.inner)
+
+    obj = MyClass()
+    obj.run()
