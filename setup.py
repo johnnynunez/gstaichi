ae2fb17c923ccdcabc7426df5e92d052b19b5edf
Hugh Perkins <hughperkins@gmail.com>
2025-07-28 15:03:06 -0400

    update python  versions

--- setup.py
+++ setup.py
@@ -32,9 +32,10 @@ classifiers = [
     "Intended Audience :: Science/Research",
     "Intended Audience :: Developers",
     "License :: OSI Approved :: Apache Software License",
-    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
 ]
 
 
