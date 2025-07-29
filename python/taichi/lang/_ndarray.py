289e2c14925383e05551fbdaebe49b4ec76311fc
Hugh Perkins <hughperkins@gmail.com>
2025-07-29 11:27:12 -0400

    precommmit

--- python/taichi/lang/_ndarray.py
+++ python/taichi/lang/_ndarray.py
@@ -254,14 +254,12 @@ class ScalarNdarray(Ndarray):
         self.element_type = dtype
 
     def __del__(self):
-        if (
-            impl is not None
-            and impl.get_runtime is not None
-            and impl.get_runtime() is not None
-        ):
-			prog = impl.get_runtime()._prog
-			if prog is not None:
-	            prog.delete_ndarray(self.arr)
+        if impl is not None:
+            runtime = impl.get_runtime()
+            if runtime is not None:
+                prog = runtime._prog
+                if prog is not None:
+                    prog.delete_ndarray(self.arr)
 
     @property
     def element_shape(self):
