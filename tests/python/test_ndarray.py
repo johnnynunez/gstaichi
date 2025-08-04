5925d15e5920d7eadc7dd5fe4495e6d7940a3350
Hugh Perkins <hughperkins@gmail.com>
2025-08-04 15:30:42 -0400

    [vulkan] Fix exception for max ndarray args test on mac vulkan (#122)

--- tests/python/test_ndarray.py
+++ tests/python/test_ndarray.py
@@ -1,4 +1,5 @@
 import copy
+import platform
 
 import numpy as np
 import pytest
@@ -1173,6 +1174,9 @@ import taichi as ti
 def my_kernel({args}) -> None:
 {arg_uses}
 """
+    if platform.system() == "Darwin" and ti.lang.impl.get_runtime().prog.config().arch == ti.vulkan:
+        # Mac doesn't support so many arguments, on Vulkan
+        return
     args_l = []
     arg_uses_l = []
     arg_objs_l = []
