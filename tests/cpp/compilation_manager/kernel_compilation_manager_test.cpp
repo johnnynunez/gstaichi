0485cf2270a3a2b8de233358a971106c17a6c040
Hugh Perkins <hughperkins@gmail.com>
2025-08-07 10:44:06 -0400

    empty kernel compilaton manager test

---
+++ tests/cpp/compilation_manager/kernel_compilation_manager_test.cpp
@@ -0,0 +1,10 @@
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "gstaichi/common/core.h"
+
+TEST(KernelCompilationManager, Basic) {
+    std::cout << "KernelCompilationManager test started." << std::endl;
+}
