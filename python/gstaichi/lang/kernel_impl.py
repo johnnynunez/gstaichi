269e519c13c0ae6e812ac3a364d477d0ab9fb1c5
Hugh Perkins <hughperkins@gmail.com>
2025-08-10 14:32:31 -0400

    add back in arch name

--- python/gstaichi/lang/kernel_impl.py
+++ python/gstaichi/lang/kernel_impl.py
@@ -714,7 +714,7 @@ class Kernel:
         if self.gstaichi_callable:
             if self.gstaichi_callable.is_pure:
                 # print("pure function:", self.func.__name__)
-                self.fast_checksum = src_hasher.hash_source(self.func, args)
+                self.fast_checksum = src_hasher.hash_source(self.func, args) + impl.current_cfg().arch.name
                 # self.fast_checksum = function_hacher.hash_kernel(self.func) + impl.current_cfg().arch.name
                 # if self.func.__name__ not in ["ndarray_to_ext_arr", "ext_arr_to_ndarray", "ndarray_matrix_to_ext_arr", "ext_arr_to_ndarray_matrix"]:
                 # print('fast_checksum', self.fast_checksum)
