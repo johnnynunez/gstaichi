3908b89bdcc7e2c1cca8b26f226ee2ff41254107
Hugh Perkins <hughperkins@gmail.com>
2025-08-10 18:28:12 -0400

    add torch tensor and ndarray

--- python/gstaichi/lang/fast_caching/args_hasher.py
+++ python/gstaichi/lang/fast_caching/args_hasher.py
@@ -6,12 +6,14 @@ import numpy as np
 from gstaichi.lang._ndarray import ScalarNdarray
 from gstaichi.lang.matrix import VectorNdarray, MatrixNdarray, MatrixField
 from gstaichi.lang.field import ScalarField
+import torch
 
 
 g_num_calls = 0
 g_num_args = 0
 g_hashing_time = 0
 g_repr_time = 0
+g_num_ignored_calls = 0
 
 
 def to_representative_str(arg: Any) -> str | None:
@@ -22,15 +24,19 @@ def to_representative_str(arg: Any) -> str | None:
     """
     arg_type = type(arg)
     if isinstance(arg, ScalarNdarray):
-        return f"[ndarray-{arg.dtype}-{len(arg.shape)}]"
+        return f"[nd-{arg.dtype}-{len(arg.shape)}]"
     if isinstance(arg, VectorNdarray):
-        return f"[ndarray-vec-{arg.n}-{arg.dtype}-{len(arg.shape)}]"
+        return f"[ndv-{arg.n}-{arg.dtype}-{len(arg.shape)}]"
     if isinstance(arg, ScalarField):
-        return f"[field-{arg.snode._id}-{arg.dtype}-{arg.shape}]"
+        return f"[f-{arg.snode._id}-{arg.dtype}-{arg.shape}]"
     if isinstance(arg, MatrixNdarray):
-        return f"[ndarray-vec-{arg.m}-{arg.n}-{arg.dtype}-{len(arg.shape)}]"
+        return f"[ndm-{arg.m}-{arg.n}-{arg.dtype}-{len(arg.shape)}]"
+    if isinstance(arg, torch.Tensor):
+        return f"[pt-{arg.dtype}-{arg.ndim}]"
+    if isinstance(arg, np.ndarray):
+        return f"[np-{arg.dtype}-{arg.ndim}]"
     if isinstance(arg, MatrixField):
-        return f"[field-{arg.m}-{arg.n}-{arg.snode._id}-{arg.dtype}-{arg.shape}]"
+        return f"[fm-{arg.m}-{arg.n}-{arg.snode._id}-{arg.dtype}-{arg.shape}]"
     if arg_type in [int, float, np.float32, np.float64, np.int32, np.int64]:
         return str(arg_type)
     print("arg not recognized", type(arg))
@@ -39,7 +45,7 @@ def to_representative_str(arg: Any) -> str | None:
 
 
 def hash_args(args: Sequence[Any]) -> str | None:
-    global g_num_calls, g_num_args, g_hashing_time, g_repr_time
+    global g_num_calls, g_num_args, g_hashing_time, g_repr_time, g_num_ignored_calls
     g_num_calls += 1
     g_num_args += len(args)
     hash_l = []
@@ -48,6 +54,7 @@ def hash_args(args: Sequence[Any]) -> str | None:
         _hash = to_representative_str(arg)
         g_repr_time += time.time() - start
         if not _hash:
+            g_num_ignored_calls += 1
             return None
         hash_l.append(_hash)
     start = time.time()
@@ -59,6 +66,7 @@ def hash_args(args: Sequence[Any]) -> str | None:
 def dump_stats() -> None:
     print("args hasher dump stats")
     print("total calls", g_num_calls)
+    print("ignored calls", g_num_ignored_calls)
     print("total args", g_num_args)
     print("hashing time", g_hashing_time)
     print("arg representation time", g_repr_time)
