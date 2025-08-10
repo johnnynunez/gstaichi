ed493ef5e7368a405c7a43b55a9695543f4da2a1
Hugh Perkins <hughperkins@gmail.com>
2025-08-10 14:02:03 -0400

    add field matrix

--- tests/python/gstaichi/lang/fast_caching/test_args_hasher.py
+++ tests/python/gstaichi/lang/fast_caching/test_args_hasher.py
@@ -156,6 +156,34 @@ def test_args_hasher_field_vector() -> None:
 
 
 @test_utils.test()
+def test_args_hasher_field_matrix() -> None:
+    seen = set()
+    for dtype in [ti.i32, ti.i64, ti.f32, ti.f64]:
+        for m in [2, 3]:
+            for n in [2, 3]:
+                for shape in [(2,), (5,), (2, 5)]:
+                    for _it in range(3):
+                        if _it == 0:
+                            _ti_init_same_arch()
+                            arg = ti.Matrix.field(m, n, dtype, shape)
+                            hash = args_hasher.hash_args([arg])
+                            assert hash is not None
+                            assert hash not in seen
+                            seen.add(hash)
+                        elif _it == 1:
+                            _ti_init_same_arch()
+                            arg = ti.Matrix.field(m, n, dtype, shape)
+                            hash = args_hasher.hash_args([arg])
+                            assert hash is not None
+                            assert hash in seen
+                        else:
+                            arg = ti.Matrix.field(m, n, dtype, shape)
+                            hash = args_hasher.hash_args([arg])
+                            assert hash not in seen
+                            seen.add(hash)
+
+
+@test_utils.test()
 def test_args_hasher_field_vs_ndarray() -> None:
     a_ndarray = ti.ndarray(ti.i32, 1)
     a_field = ti.field(ti.i32, 1)
