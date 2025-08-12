bdc1ec54361c1d24b9ec1a0374e8670f0b67ca39
Hugh Perkins <hughperkins@gmail.com>
2025-08-12 08:41:27 -0400

    add test of store and valdiate for src hasher

--- tests/python/gstaichi/lang/fast_caching/test_src_hasher.py
+++ tests/python/gstaichi/lang/fast_caching/test_src_hasher.py
@@ -1,3 +1,6 @@
+import pathlib
+import shutil
+from typing import Callable
 import gstaichi as ti
 from tests import test_utils
 import sys
@@ -54,3 +57,78 @@ def test_create_cache_key_vary_fn(monkeypatch) -> None:
     assert key_base != ""
     assert key_base == key_same
     assert key_base != key_diff
+
+
+@test_utils.test()
+def test_function_hasher_validate_hashed_function_infos(monkeypatch, tmp_path: pathlib.Path) -> None:
+    test_files_path = pathlib.Path("tests/python/gstaichi/lang/fast_caching/test_files")
+    monkeypatch.syspath_prepend(str(tmp_path))
+
+    def setup_folder(filename: str) -> None:
+        shutil.copy2(test_files_path / filename, tmp_path / "child_diff.py")
+
+    setup_folder("child_diff_base.py")
+    mod = importlib.import_module("child_diff")
+
+    def get_fileinfos(functions: list[Callable]) -> list[_wrap_inspect.FunctionSourceInfo]:
+        fileinfos = []
+        for f in functions:
+            file_info, _src = _wrap_inspect.get_source_info_and_src(f)
+            fileinfos.append(file_info)
+        return fileinfos
+
+    fileinfos = get_fileinfos([mod.f1.fn, mod.f2.fn])
+    hashed_fileinfos = function_hasher.hash_functions(fileinfos)
+    assert function_hasher.validate_hashed_function_infos(hashed_fileinfos)
+
+    setup_folder("child_diff_same.py")
+    assert function_hasher.validate_hashed_function_infos(hashed_fileinfos)
+
+    setup_folder("child_diff_diff.py")
+    assert not function_hasher.validate_hashed_function_infos(hashed_fileinfos)
+
+    setup_folder("child_diff_same.py")
+    assert function_hasher.validate_hashed_function_infos(hashed_fileinfos)
+
+
+@test_utils.test()
+def test_src_hasher_store_validate(monkeypatch, tmp_path) -> None:
+    test_files_path = pathlib.Path("tests/python/gstaichi/lang/fast_caching/test_files")
+
+    offline_cache_path = tmp_path / "cache"
+    temp_import_path = tmp_path / "temp_import"
+    temp_import_path.mkdir(exist_ok=True)
+
+    ti_init_same_arch(options={"offline_cache_file_path": str(offline_cache_path)})
+
+    monkeypatch.syspath_prepend(temp_import_path)
+
+    def setup_folder(filename: str) -> None:
+        shutil.copy2(test_files_path / filename, temp_import_path / "child_diff.py")
+
+    def get_fileinfos(functions: list[Callable]) -> list[_wrap_inspect.FunctionSourceInfo]:
+        fileinfos = []
+        for f in functions:
+            file_info, _src = _wrap_inspect.get_source_info_and_src(f)
+            fileinfos.append(file_info)
+        return fileinfos
+
+    setup_folder("child_diff_base.py")
+    mod = importlib.import_module("child_diff")
+    kernel_info = get_fileinfos([mod.f1.fn])[0]
+    fileinfos = get_fileinfos([mod.f1.fn, mod.f2.fn])
+    cache_key = src_hasher.create_cache_key(kernel_info, [])
+
+    src_hasher.store(cache_key, fileinfos)
+    assert src_hasher.validate_cache_key(cache_key)
+
+    setup_folder("child_diff_same.py")
+    assert src_hasher.validate_cache_key(cache_key)
+
+    setup_folder("child_diff_diff.py")
+    assert not src_hasher.validate_cache_key(cache_key)
+
+    setup_folder("child_diff_same.py")
+    assert src_hasher.validate_cache_key(cache_key)
+
+    assert not src_hasher.validate_cache_key("abcdefg")
