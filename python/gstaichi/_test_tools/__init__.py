c00a1274ad28c5b99151d27438f1eac3e200fd10
Hugh Perkins <hughperkins@gmail.com>
2025-08-12 08:11:15 -0400

    add test that src hasher cache key depends on config

---
+++ python/gstaichi/_test_tools/__init__.py
@@ -0,0 +1,9 @@
+from typing import Any
+import gstaichi as ti
+
+
+def ti_init_same_arch(options: dict[str, Any] | None = None) -> None:
+    assert ti.cfg is not None
+    if not options:
+        options = {}
+    ti.init(arch=getattr(ti, ti.cfg.arch.name), **options)
