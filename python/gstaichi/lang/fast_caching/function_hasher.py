9621cdd26679c59daef0f6fbd28cef3d18c778a6
Hugh Perkins <hughperkins@gmail.com>
2025-08-12 02:38:20 -0400

    factorize hash a bit

--- python/gstaichi/lang/fast_caching/function_hasher.py
+++ python/gstaichi/lang/fast_caching/function_hasher.py
@@ -12,18 +12,23 @@ def pure(fn: "GsTaichiCallable") -> "GsTaichiCallable":
     return fn
 
 
+def _hash_funcion(function_info: FunctionSourceInfo) -> str:
+    with open(function_info.filepath) as f:
+        contents = f.read().split("\n")
+    lines = contents[function_info.start_lineno: function_info.end_lineno]
+    _hash = hashlib.sha256("\n".join(lines).encode('utf-8')).hexdigest()
+    return _hash
+
+
 def hash_functions(function_infos: Iterable[FunctionSourceInfo]) -> list[tuple[FunctionSourceInfo, str]]:
     results = []
     for f_info in function_infos:
-        # print('-------------------------')
-        with open(f_info.filepath) as f:
-            contents = f.read().split("\n")
-        lines = contents[f_info.start_lineno: f_info.end_lineno]
-        # print('lines[')
-        # print("\n".join(lines))
-        # print(']')
-        _hash = hashlib.sha256("\n".join(lines).encode('utf-8')).hexdigest()
+        _hash = _hash_funcion(f_info)
         print(f_info.function_name, _hash)
         results.append((f_info, _hash))
     print("len(function_infos)", len(results))
     return results
+
+
+def hash_kernel(kernel_info: FunctionSourceInfo) -> str:
+    return _hash_funcion(kernel_info)
