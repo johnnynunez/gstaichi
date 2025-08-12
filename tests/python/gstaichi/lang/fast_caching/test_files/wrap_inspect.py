dcedeea0580dbed3cc14a5a2acd40d5f70560bc2
Hugh Perkins <hughperkins@gmail.com>
2025-08-12 07:13:44 -0400

    test wrap_inspect

---
+++ tests/python/gstaichi/lang/fast_caching/test_files/wrap_inspect.py
@@ -0,0 +1,16 @@
+
+
+
+# from 5 to 8, 4 lines
+def my_func(a: int, b: int) -> int:
+    a += 5
+    b += a
+    return a + b
+
+
+class Foo:
+    # from 13 to 16, 4 lines
+    def my_func2(self, a: int, b: int) -> int:
+        a += 5
+        b += a
+        return a + b
---
+++ tests/python/gstaichi/lang/test_wrap_inspect.py
@@ -0,0 +1,43 @@
+from gstaichi.lang import _wrap_inspect
+from tests import test_utils
+import sys
+import importlib
+
+
+
+@test_utils.test()
+def test_wrap_inspect() -> None:
+    test_files_path = "tests/python/gstaichi/lang/fast_caching/test_files"
+    if test_files_path not in sys.path:
+        sys.path.append(test_files_path)
+
+    wrap_inspect = importlib.import_module("wrap_inspect")
+    source_info, src = _wrap_inspect.get_source_info_and_src(wrap_inspect.my_func)
+    assert source_info.function_name == "my_func"
+    assert source_info.start_lineno == 5
+    assert source_info.end_lineno == 8
+    assert len(src) == 4
+    assert all([line != "" and len(line) > 2 for line in src])
+    actual_src = "".join(src)
+    expected = """def my_func(a: int, b: int) -> int:
+    a += 5
+    b += a
+    return a + b
+"""
+    assert actual_src == expected
+
+    source_info, src = _wrap_inspect.get_source_info_and_src(wrap_inspect.Foo.my_func2)
+    assert source_info.function_name == "my_func2"
+    assert source_info.start_lineno == 13
+    assert source_info.end_lineno == 16
+    assert len(src) == 4
+    assert all([line != "" and len(line) > 2 for line in src])
+    actual_src = "".join(src)
+    expected = """    def my_func2(self, a: int, b: int) -> int:
+        a += 5
+        b += a
+        return a + b
+"""
+    assert actual_src == expected
+
+    sys.path.remove(test_files_path)
