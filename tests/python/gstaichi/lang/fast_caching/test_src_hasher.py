3bb00b9e996acf428c9c0f7d78db0e2e08191b6d
Hugh Perkins <hughperkins@gmail.com>
2025-08-12 08:28:03 -0400

    cache key fn  test

--- tests/python/gstaichi/lang/fast_caching/test_src_hasher.py
+++ tests/python/gstaichi/lang/fast_caching/test_src_hasher.py
@@ -33,34 +33,24 @@ def test_create_cache_key_vary_config() -> None:
 
 
 @test_utils.test()
-def test_create_cache_key_vary_fn() -> None:
-    import sys
+def test_create_cache_key_vary_fn(monkeypatch) -> None:
     test_files_path = "tests/python/gstaichi/lang/fast_caching/test_files"
-    if test_files_path not in sys.path:
-        sys.path.append(test_files_path)
+    monkeypatch.syspath_prepend(test_files_path)
 
-    def get_infos(name: str) -> list[HashedFunctionSourceInfo]:
+    def get_cache_key(name: str) -> _wrap_inspect.FunctionSourceInfo:
         mod = importlib.import_module(name)
         info, _src = _wrap_inspect.get_source_info_and_src(mod.f1.fn)
-        hashed_infos = function_hasher.hash_functions([info])
-        return hashed_infos
+        cache_key = src_hasher.create_cache_key(info, [])
+        return cache_key
 
-    f1_base_hashed_infos = get_infos("f1_base")
-    f1_same_hashed_infos = get_infos("f1_same")
-    f1_diff_hashed_infos = get_infos("f1_diff")
+    key_base = get_cache_key("f1_base")
+    print(key_base)
+    key_same = get_cache_key("f1_same")
+    print(key_same)
+    key_diff = get_cache_key("f1_diff")
+    print(key_diff)
 
-
-    ti_init_same_arch()
-    kernel_info, _src = get_source_info_and_src(f1.fn)
-    cache_key_base = src_hasher.create_cache_key(kernel_info, [])
-
-    ti_init_same_arch()
-    kernel_info, _src = get_source_info_and_src(f1.fn)
-    cache_key_same = src_hasher.create_cache_key(kernel_info, [])
-
-    ti_init_same_arch(options={"offline_cache_max_size_of_files": 123})
-    kernel_info, _src = get_source_info_and_src(f1.fn)
-    cache_key_diff = src_hasher.create_cache_key(kernel_info, [])
-
-    assert cache_key_base == cache_key_same
-    assert cache_key_same != cache_key_diff
+    assert key_base is not None
+    assert key_base != ""
+    assert key_base == key_same
+    assert key_base != key_diff
