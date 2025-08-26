4467a62b97aa16e4f2cf3217eadaf20338356f4d
Hugh Perkins <hughperkins@gmail.com>
2025-08-26 18:34:12 -0400

    add test for repeated calls to function laoded from cache

--- tests/python/gstaichi/lang/fast_caching/test_src_ll_cache.py
+++ tests/python/gstaichi/lang/fast_caching/test_src_ll_cache.py
@@ -37,3 +37,27 @@ def test_src_ll_cache1(tmp_path: pathlib.Path) -> None:
     assert has_pure._primal.src_ll_cache_observations.cache_key_generated
     assert has_pure._primal.src_ll_cache_observations.cache_validated
     assert has_pure._primal.src_ll_cache_observations.cache_loaded
+
+
+@test_utils.test()
+def test_src_ll_cache_repeat_after_load(tmp_path: pathlib.Path) -> None:
+    ti_init_same_arch(offline_cache_file_path=str(tmp_path), offline_cache=True)
+
+    @ti.pure
+    @ti.kernel
+    def has_pure(a: ti.types.NDArray[ti.i32, 1]) -> None:
+        a[0] += 1
+
+    ti_init_same_arch(offline_cache_file_path=str(tmp_path), offline_cache=True)
+    a = ti.ndarray(ti.i32, (10, ))
+    a[0] = 5
+    for i in range(3):
+        has_pure(a)
+        assert a[0] == 6 + i
+
+    ti_init_same_arch(offline_cache_file_path=str(tmp_path), offline_cache=True)
+    a = ti.ndarray(ti.i32, (10, ))
+    a[0] = 5
+    for i in range(3):
+        has_pure(a)
+        assert a[0] == 6 + i
