39c6c256832713883facb54be2b1a319bbcf87c2
Hugh Perkins <hughperkins@gmail.com>
2025-08-26 08:01:49 -0400

    [Type] Revise the ndarray annotations on test_ndarray.py (#134)

--- python/gstaichi/lang/kernel_impl.py
+++ python/gstaichi/lang/kernel_impl.py
@@ -667,6 +667,9 @@ class Kernel:
                     ),
                 ):
                     pass
+                elif annotation is ndarray_type.NdarrayType:
+                    # convert from ti.types.NDArray into ti.types.NDArray()
+                    annotation = annotation()
                 elif id(annotation) in primitive_types.type_ids:
                     pass
                 elif isinstance(annotation, sparse_matrix_builder):
