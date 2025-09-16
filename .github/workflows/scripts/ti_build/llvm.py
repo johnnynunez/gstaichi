606e686f0c46889a8343be64756db01badebdd30
Hugh Perkins <hughperkins@gmail.com>
2025-09-16 08:04:16 -0400

    factorize out llvm build url a bit

--- .github/workflows/scripts/ti_build/llvm.py
+++ .github/workflows/scripts/ti_build/llvm.py
@@ -19,25 +19,28 @@ def setup_llvm() -> None:
     Download and install LLVM.
     """
     u = platform.uname()
+
+    release_url_template = "https://github.com/Genesis-Embodied-AI/gstaichi-sdk-builds/releases/download/llvm-15.0.7-hp-johnny-minus-mlir-202509152023/taichi-llvm-15.0.7-{platform}.zip"
+
     if (u.system, u.machine) == ("Linux", "x86_64"):
         if cmake_args.get_effective("TI_WITH_AMDGPU"):
             out = get_cache_home() / "llvm15-amdgpu-005"
             url = "https://github.com/GaleSeLee/assets/releases/download/v0.0.5/taichi-llvm-15.0.0-linux.zip"
         else:
             out = get_cache_home() / "llvm15.0.7-x86"
-            url = "https://github.com/Genesis-Embodied-AI/gstaichi-sdk-builds/releases/download/llvm-15.0.7-hp-johnny-minus-mlir-202509152023/taichi-llvm-15.0.7-linux-x86_64.zip"
+            url = release_url_template.format(platform="linux-x86_64")
         download_dep(url, out, strip=1)
     elif (u.system, u.machine) in (("Linux", "arm64"), ("Linux", "aarch64")):
         out = get_cache_home() / "llvm-arm-15.0.7"
-        url = "https://github.com/Genesis-Embodied-AI/gstaichi-sdk-builds/releases/download/llvm-15.0.7-hp-johnny-minus-mlir-202509152023/taichi-llvm-15.0.7-linux-aarch64.zip"
+        url = release_url_template.format(platform="linux-aarch64")
         download_dep(url, out, strip=1)
     elif (u.system, u.machine) == ("Darwin", "arm64"):
         out = get_cache_home() / "llvm15-m1-nozstd"
-        url = "https://github.com/Genesis-Embodied-AI/gstaichi-sdk-builds/releases/download/llvm-15.0.7-hp-johnny-minus-mlir-202509152023/taichi-llvm-15.0.7-macos-arm64.zip"
+        url = release_url_template.format(platform="macos-arm64")
         download_dep(url, out, strip=1)
     elif (u.system, u.machine) == ("Windows", "AMD64"):
         out = get_cache_home() / "llvm15"
-        url = "https://github.com/Genesis-Embodied-AI/gstaichi-sdk-builds/releases/download/llvm-15.0.7-hp-johnny-minus-mlir-202509152023/taichi-llvm-15.0.7-windows-amd64.zip"
+        url = release_url_template.format(platform="windows-amd64")
         download_dep(url, out, strip=0)
     else:
         raise RuntimeError(f"Unsupported platform: {u.system} {u.machine}")
