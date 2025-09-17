7801c2f3abf02aa8118bc4ec4e0510e3c1c28b60
Johnny <johnnynuca14@gmail.com>
2025-09-17 03:13:12 +0200

    Update entry.py

--- .github/workflows/scripts/ti_build/entry.py
+++ .github/workflows/scripts/ti_build/entry.py
@@ -33,11 +33,11 @@ def build_wheel(python: Command, pip: Command) -> None:
     cmake_args.writeback()
     u = platform.uname()
     if (u.system, u.machine) == ("Linux", "x86_64"):
-        extra.extend(["-p", "manylinux_2_27_x86_64"])
+        extra.extend(["-p", "manylinux_2_34_x86_64"])
     elif (u.system, u.machine) in (("Linux", "arm64"), ("Linux", "aarch64")):
-        extra.extend(["-p", "manylinux_2_27_aarch64"])
+        extra.extend(["-p", "manylinux_2_34_aarch64"])
     if platform.system() == "Darwin":
-        extra.extend(["-p", "macosx-11.0-arm64"])
+        extra.extend(["-p", "macosx-14.0-arm64"])
 
     python("setup.py", "clean")
     python("misc/make_changelog.py", "--ver", "origin/main", "--repo_dir", "./", "--save")
